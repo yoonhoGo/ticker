1c903e7ac072bc3134c1ea850402dd2446edbf23
Yoonho Go <lylisha@gmail.com>
2019-12-18 15:11:20 +0900

    예제 코드 반영

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use chrono::{TimeZone, Utc};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
-use std::net::SocketAddr;
 
 fn tick(nano: &mut u32) -> i64 {
     let utc = Utc::now();
@@ -28,25 +27,26 @@ async fn get_tick(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
 }
 
 #[tokio::main]
-async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // For every connection, we must make a `Service` to handle all
+    // incoming HTTP requests on said connection.
     let make_svc = make_service_fn(|_conn| {
-        async {
-            // service_fn converts our function into a `Service`
-            Ok::<_, Infallible>(service_fn(get_tick))
-        }
+        // This is the `Service` that will handle the connection.
+        // `service_fn` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        async { Ok::<_, Infallible>(service_fn(get_tick)) }
     });
 
-    println!("Server is run");
+    let addr = ([127, 0, 0, 1], 3000).into();
 
     let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
 
     // Run this server for... forever!
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
+
+    Ok(())
 }
